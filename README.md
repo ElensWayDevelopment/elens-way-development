6fa2bcfe719a254e80bebd5f8acf44c83b85c651
Alex Kurch <alexkurch100@gmail.com>
2024-03-05 11:12:04 +0200

    feat(main): basic setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MentalHealth
+# Elens Way
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mental-health' title`, () => {
+  it(`should have the 'elens-way' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mental-health');
+    expect(app.title).toEqual('elens-way');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mental-health');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, elens-way');
   });
 });
