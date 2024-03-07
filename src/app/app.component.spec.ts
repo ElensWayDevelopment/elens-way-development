9de2758a8d9250907b71824b944cfc5fd20d0624
Alex Kurch <alexkurch100@gmail.com>
2024-03-07 23:17:42 +0200

    feat(main): drawer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent]
     }).compileComponents();
   });
 
@@ -14,16 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'elens-way' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('elens-way');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, elens-way');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, elens-way'
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,91 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { DomSanitizer } from '@angular/platform-browser';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    HttpClientModule
+  ],
+  providers: [MatIconRegistry],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'elens-way';
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'hamburger-4',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/hamburger-4.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'arrow-bottom-right',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/arrow-bottom-right.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'instagram',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/instagram.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'telegram',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/telegram.svg')
+    );
+  }
+  menuItems = [
+    {
+      name: 'Консультации',
+      link: '/'
+    },
+    {
+      name: `Подкаст
+      «Ты — это важно»`,
+      link: '/'
+    },
+    {
+      name: `YouTube-канал`,
+      link: '/'
+    },
+    {
+      name: `Портал психологов
+      «Ты — это важно»`,
+      link: '/'
+    },
+    {
+      name: 'Командные тренинги',
+      link: '/'
+    }
+  ];
+
+  footerItems = [
+    {
+      name: '@elens_way',
+      link: '/',
+      iconName: 'instagram'
+    },
+    {
+      name: '@elens_way',
+      link: '/',
+      iconName: 'telegram'
+    }
+  ];
 }
