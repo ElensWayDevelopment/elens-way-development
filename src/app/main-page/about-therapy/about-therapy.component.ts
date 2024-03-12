2a963ea2c1d3af70e3aa2ff62e62cc80d90cb061
Alex Kurch <alexkurch100@gmail.com>
2024-03-12 22:59:31 +0200

    feat: build v10

--- src/app/main-page/about-therapy/about-therapy.component.ts
+++ src/app/main-page/about-therapy/about-therapy.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-therapy',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './about-therapy.component.html',
   styleUrl: './about-therapy.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
